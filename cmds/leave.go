746ffba3750e6f14890e60af3c8292500160a8db
earthian <d3v1ant@mail.ru>
2023-05-13 20:40:04 +0400

    fix leave command, update todos

--- cmds/leave.go
+++ cmds/leave.go
@@ -13,11 +13,12 @@ func Leave(ctx Ctx) {
 	if ctx.Stream.V != nil {
 		// TODO: here (or maybe in leave command ye), and on 'join' event play
 		// some japanese sounds like 'hi' and 'bye' , better from anime
-		ctx.Stream.Reset(false)
 		err := ctx.Stream.V.Disconnect()
 		if err != nil {
 			fmt.Println("Error leaving voice channel:", err)
+			return
 		}
+		ctx.Stream.Reset(false)
 	} else {
 		ctx.Reply("You can check out any time you like, but you can never leave.")
 		return
