f7ba0dcda3165daecbac4e11b3321c2a94a716f0
earthian <d3v1ant@mail.ru>
2023-05-20 11:34:55 +0400

    move reply to user for repeat f-n to cmds pkg

--- cmds/repeat.go
+++ cmds/repeat.go
@@ -9,6 +9,19 @@ func Repeat(ctx Ctx) {
 		return
 	}
 
-	reponse := ctx.Stream.SetRepeat(ctx.Args)
-	ctx.Reply(reponse)
+	err = ctx.Stream.SetRepeat(ctx.Args)
+	if err != nil {
+		ctx.Reply(fmt.Sprintf("Usage: **%srepeat single | all | off**", ctx.Prefix))
+		return
+	}
+	// kinda hardcode but user responses should be handled here, not in Stream
+	switch ctx.Args {
+	case "single":
+		ctx.Reply(fmt.Sprintf("Now repeating: **%s**", ctx.Stream.Current()))
+	case "all":
+		ctx.Reply(
+			fmt.Sprintf("Now repeating **%s** songs", fmt.Sprint(len(ctx.Stream.Queue))))
+	case "off":
+		ctx.Reply("Repeat turned off")
+	}
 }
