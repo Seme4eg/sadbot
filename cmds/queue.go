54562b88122681625f867dd417c87315077cc517
earthian <d3v1ant@mail.ru>
2023-05-20 11:34:24 +0400

    change queue text formatting + pagination fix

--- cmds/queue.go
+++ cmds/queue.go
@@ -2,6 +2,7 @@ package cmds
 
 import (
 	"fmt"
+	"math"
 	"sadbot/paginator"
 
 	"github.com/bwmarrin/discordgo"
@@ -15,31 +16,28 @@ func Queue(ctx Ctx) {
 
 	p := paginator.NewPaginator(ctx.S, ctx.M.ChannelID)
 
-	// 10 tracks per page
-	perPage := 10
+	// 10 tracks per page (starting with 0 index)
+	perPage := 9
 	// needed for further format function, whether to number tracks 01 or 001
 	numLen := len(fmt.Sprint(len(ctx.Stream.Queue)))
 	formatNum := "%0" + fmt.Sprint(numLen) + "d "
+	var page int
 
 	for i := 0; i < len(ctx.Stream.Queue); i += perPage {
 
-		var maxLen int
+		embed := &discordgo.MessageEmbed{Title: "Current queue"}
 
-		embed := &discordgo.MessageEmbed{
-			Title: "Current queue",
-			// Color: 0x00ff00,
-		}
-
-		if a, b := i+perPage, len(ctx.Stream.Queue)-1; a > b {
-			maxLen = b
-		} else {
-			maxLen = a
-		}
+		maxLen := int(math.Min(
+			float64(i+perPage),
+			float64(len(ctx.Stream.Queue)-1)))
 
 		for j := i; j <= maxLen; j++ {
 			formatName := "**%s** – %s"
+			// change page & format for current song
 			if j == ctx.Stream.SongIndex {
 				formatName = "**%s** – **%s**"
+				// page = int(math.Ceil(float64(j) / float64(perPage)))
+				page = j / perPage
 			}
 			field := discordgo.MessageEmbedField{
 				Value: fmt.Sprintf(formatName,
@@ -48,6 +46,10 @@ func Queue(ctx Ctx) {
 			embed.Fields = append(embed.Fields, &field)
 		}
 
+		// to start showing next page from new index and not repeat last song
+		// from previous page
+		i++
+
 		p.Add(embed)
 	}
 
@@ -55,5 +57,5 @@ func Queue(ctx Ctx) {
 	// i can add custom handlers here like play / pause / stop / clear / save
 	// p.Handle("+", func() { ctx.S.ChannelMessageSend(ctx.M.ChannelID, "Hi!") })
 
-	p.Spawn()
+	p.Spawn(page)
 }
